ee965d3499bd57c1626488bc2bb0cf27b47cee56
Terbinm <a0978672@gmail.com>
2026-01-16 07:14:12 +0800

    feat: add support for displaying supported audio device bit depths and implement eventlet monkey patching

--- core/state_management/state_management_main.py
+++ core/state_management/state_management_main.py
@@ -2,7 +2,13 @@
 狀態管理系統主應用
 提供 REST API 和 Web UI 管理界面
 """
+# Eventlet monkey patching 必須在所有其他導入之前執行
+# 這是為了確保 pymongo 等庫能正確使用 eventlet 的非同步 I/O
 import os
+if os.environ.get('WEBSOCKET_ASYNC_MODE') == 'eventlet':
+    import eventlet
+    eventlet.monkey_patch()
+
 import sys
 import logging
 import threading
